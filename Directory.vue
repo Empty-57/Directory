e172184a2a474dffdb6435a35032592708fab3d1
Empty <79520820+Empty-57@users.noreply.github.com>
2024-11-08 15:43:58 +0800

    Update Directory.vue

--- Directory.vue
+++ Directory.vue
@@ -66,12 +66,9 @@ export default {
 </script>
 
 <template>
-  <HomeMenu></HomeMenu>
-  <ToTop></ToTop>
     <div id="directory" class="sticky top-16 min-w-60 w-fit opacity-0 duration-500 bg-zinc-300/60 dark:bg-zinc-900/60 rounded shadow shadow-zinc-400/60 dark:shadow-zinc-950/60 overflow-y-auto overflow-x-hidden max-h-[80svh] self-start mx-4 hidden lg:block" :class="{'opacity-100':directory_flag}">
       <div class="sticky top-0 bg-zinc-300 dark:bg-zinc-900 w-full self-start p-4 py-2 border-b-[1px] border-zinc-700">目录</div>
     </div>
-
 </template>
 <style>
   .anchor_hl{

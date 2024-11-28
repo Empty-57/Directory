33770b1310e3eb0c9b4cc0685d3cc7a635b9e50b
Empty <79520820+Empty-57@users.noreply.github.com>
2024-11-28 19:06:41 +0800

    Update Directory.vue

--- Directory.vue
+++ Directory.vue
@@ -1,14 +1,17 @@
 <script setup>
 import {h, onMounted, ref, render} from "vue";
 import {debounce} from "@/assets/js/debounce.js";
+import {Expo, gsap} from "gsap";
+import {ScrollToPlugin} from "gsap/ScrollToPlugin";
 
+gsap.registerPlugin(ScrollToPlugin);
 const directory_flag = ref(false)
 
 function anchor_highlight(anchor) {
   if (!anchor) {
     return;
   }
-  const anchor_list = document.querySelectorAll('a.anchor_hl')
+  const anchor_list = document.querySelectorAll('span.anchor_hl')
   if (anchor_list) {
     anchor_list.forEach(item => {
       item.classList.remove('anchor_hl')
@@ -19,7 +22,7 @@ function anchor_highlight(anchor) {
 
 onMounted(() => {
   let count = 0;
-  const timer3 = setInterval(() => {
+  const timer = setInterval(() => {
     const article_a = document.querySelectorAll('h1,h2')
     if (article_a) {
       count = 0;
@@ -30,32 +33,55 @@ onMounted(() => {
       })
       window.addEventListener('scroll', debounce(() => {
         let hl_link;
-        for (let i of article_a) {
-          let top = i.getBoundingClientRect().top;
-          if (top >= 0 && top <= 500) {
-            hl_link = document.querySelector('[href="#' + i.id + '"]');
-            if (hl_link) {
-              anchor_highlight(hl_link)
-            }
+        for (let i = 0; i < article_a.length; i++) {
+          let top = article_a[i].getBoundingClientRect().top;
+          if (top >= 0 && top <= 300) {
+            hl_link = document.querySelector('[id="#' + article_a[i].id + '"]');
+            anchor_highlight(hl_link)
+            break;
+          }
+          if (top < 0 && article_a[i + 1] && article_a[i + 1].getBoundingClientRect().top > document.documentElement.clientHeight) {
+            hl_link = document.querySelector('[id="#' + article_a[i].id + '"]');
+            anchor_highlight(hl_link)
+            break;
+          }
+          if (top < 0 && article_a[i + 1] && article_a[i + 1].getBoundingClientRect().top < document.documentElement.clientHeight && article_a[i + 1].getBoundingClientRect().top > 300) {
+            hl_link = document.querySelector('[id="#' + article_a[i].id + '"]');
+            anchor_highlight(hl_link)
+            break;
+          }
+          if (top < 0 && !article_a[i + 1]) {
+            hl_link = document.querySelector('[id="#' + article_a[i].id + '"]');
+            anchor_highlight(hl_link)
             break;
           }
         }
       }, 100))
-      const renderFn = () => h('div', {class: 'gap-2 bg-translate h-fit p-4 pt-2 flex flex-col *:duration-200'}, Array.from({length: article_a.length}).map((item, index) => {
-        return h('a', {
+      const renderFn = () => h('div', {
+        id: 'anchor_box',
+        class: 'gap-2 bg-translate h-fit p-4 pt-2 flex flex-col *:duration-200'
+      }, Array.from({length: article_a.length}).map((item, index) => {
+        return h('span', {
           class: (article_a[index].tagName === 'H1' ? '' : 'ml-4 ') + 'hover:text-cyan-600 active:text-cyan-700 text-left text-pretty break-all',
-          href: "#" + article_a[index].id
+          id: "#" + article_a[index].id
         }, article_a[index].innerText)
       }))
       render(renderFn(), directory)
       directory_flag.value = true
-      anchor_highlight(document.querySelector('[href="#' + article_a[0].id + '"]'))
+      const anchors = document.querySelector('#anchor_box').childNodes
+      anchors.forEach(item => {
+        item.onclick = e => {
+          const to_elem = document.querySelector('[id="' + e.target.id.split('#')[1] + '"]')
+          gsap.to(window, {duration: 0.5, scrollTo: to_elem.offsetTop - 50, ease: Expo.easeOut})
+        }
+      })
+      anchor_highlight(document.querySelector('[id="#' + article_a[0].id + '"]'))
 
-      clearInterval(timer3)
+      clearInterval(timer)
     } else {
       count++
       if (count === 5) {
-        clearInterval(timer3);
+        clearInterval(timer);
       }
     }
   }, 500)
@@ -65,7 +91,7 @@ onMounted(() => {
 <template>
   <div id="directory"
        :class="{'opacity-100':directory_flag}"
-       class="*:text-sm sticky top-16 right-4 min-w-60 w-fit opacity-0 duration-500 bg-zinc-300/60 dark:bg-zinc-900/60 rounded shadow shadow-zinc-400/60 dark:shadow-zinc-950/60 overflow-y-auto overflow-x-hidden max-h-[80svh] self-start mx-4 hidden lg:block">
+       class="*:text-sm sticky top-16 min-w-60 w-fit opacity-0 duration-500 bg-zinc-300/60 dark:bg-zinc-900/60 rounded shadow shadow-zinc-400/60 dark:shadow-zinc-950/60 overflow-y-auto overflow-x-hidden max-h-[80svh] self-start mx-4 hidden lg:block">
     <div class="sticky top-0 bg-zinc-300 dark:bg-zinc-900 w-full self-start p-4 py-2 border-b-[1px] border-zinc-700">
       目录
     </div>
